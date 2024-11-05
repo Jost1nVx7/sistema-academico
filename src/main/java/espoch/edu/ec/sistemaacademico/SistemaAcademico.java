45bd2c39aa3106c26229b2b74929803988c0d868
Jost1nVx7 <jostinjd2002@gmail.com>
2024-11-05 10:28:40 -0500

    sistema academico

---
+++ src/main/java/espoch/edu/ec/sistemaacademico/SistemaAcademico.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.sistemaacademico;
+
+/**
+ *
+ * @author Global Technology Ec
+ */
+public class SistemaAcademico {
+
+    public static void main(String[] args) {
+        System.out.println("Jostin Torres");
+        System.out.println("facillll");
+        System.out.println("sistema academico");
+    }
+}
